a6d8a1c7137f23528939284e16b5b9958372f1c1
tee-talog <tee.talog@gmail.com>
2023-12-29 16:16:50 +0900

    create

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import VSvg from "./src/VSvg.vue"
+</script>
+
+<template>
+  <div>
+    <!-- name にファイル名（拡張子なし）を渡す -->
+    <VSvg class="icon" name="home" />
+    <VSvg class="icon" name="home" color="rgba(100, 200, 250, 0.8)" />
+    <VSvg class="icon" name="good" color="red" />
+
+    <hr />
+
+    <img src="/home.svg" class="icon" />
+    <img src="/good.svg" class="icon" />
+  </div>
+</template>
+
+<style scoped>
+.icon {
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+}
+</style>
---
+++ src/VSvg.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    name: string
+    // NOTE: 複数の色を受け取りたい SVG が必要なら再度検討
+    color?: string
+  }>(),
+  {
+    color: () => "",
+  },
+)
+
+const componentName = computed(() => `svg-${props.name}`)
+</script>
+
+<template>
+  <component :is="componentName" class="svg" />
+</template>
+
+<style scoped>
+.svg {
+  --color: v-bind(color);
+}
+</style>
